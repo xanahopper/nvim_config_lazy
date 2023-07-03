414c2105fd148261ea89633e7af78577b1396415
cuixianming <cuixianming@bytedance.com>
2023-07-03 10:28:39 +0800

    chore: update

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -7,4 +7,23 @@ return {
     config = true,
     ft = { "rust" },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {},
+    },
+  },
+  {
+    "folke/neoconf.nvim",
+    opts = {
+      import = {
+        vscode = true,
+      },
+    },
+    keys = {
+      { "<leader>sff", "<cmd>Neoconf<CR>", desc = "Neoconf" },
+      { "<leader>sfl", "<cmd>Neoconf local<CR>", desc = "Neoconf local" },
+      { "<leader>sfg", "<cmd>Neoconf global<CR>", desc = "Neoconf global" },
+    },
+  },
 }
