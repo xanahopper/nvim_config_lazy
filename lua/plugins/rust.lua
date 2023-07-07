ddfd2906cd41e4111f535c78efec4197371a8b29
cuixianming <cuixianming@bytedance.com>
2023-07-07 19:03:07 +0800

    chore: update

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -11,6 +11,13 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {},
+      tools = {
+        autoSetHints = true,
+        hover_with_actions = true,
+        inlay_hints = {
+          show_parameter_hints = true,
+        },
+      },
     },
   },
   {
